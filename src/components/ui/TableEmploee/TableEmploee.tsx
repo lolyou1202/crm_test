d5a6ceb2542dbe0320cf410d2407fef28e0d9e25
lolyou1202 <sasha555990@gmail.com>
2024-05-07 18:01:04 +0300

    фыаыв

--- src/components/ui/TableEmploee/TableEmploee.tsx
+++ src/components/ui/TableEmploee/TableEmploee.tsx
@@ -2,81 +2,24 @@ import './TableEmploee.style.scss'
 import { Star } from '../../Icons/Star'
 import { TableSort } from '../../Icons/TableSort'
 import { useState } from 'react'
-
-const columns = [
-	{
-		id: 'name',
-		label: 'ФИО',
-		minWidth: 320,
-	},
-	{
-		id: 'id',
-		label: 'Внутр. номер',
-		minWidth: 140,
-	},
-	{
-		id: 'phone',
-		label: 'Телефон',
-		minWidth: 160,
-	},
-	{
-		id: 'mail',
-		label: 'Почта',
-		minWidth: 220,
-	},
-	{
-		id: 'branch',
-		label: 'Филиал',
-		minWidth: 160,
-	},
-	{
-		id: 'department',
-		label: 'Отдел',
-		minWidth: 160,
-	},
-	{
-		id: 'post',
-		label: 'Должность',
-		minWidth: 200,
-	},
-	{
-		id: 'director',
-		label: 'Руководитель',
-		minWidth: 180,
-	},
-	{
-		id: 'experience',
-		label: 'Стаж работы (лет)',
-		minWidth: 200,
-	},
-	{
-		id: 'fullyear',
-		label: 'Полных лет',
-		minWidth: 140,
-	},
-	{
-		id: 'nationality',
-		label: 'Гражданство',
-		minWidth: 160,
-	},
-]
+import { tableColumns, tableRows } from '../../../constants/tableFields'
 
 export const TableEmploee = () => {
 	const [hoverColumn, setHoverColumn] = useState<number | null>(null)
 
 	return (
-		<div className='table'>
-			<div className='table-head'>
-				<div className='table-row'>
-					<div
-						className='table-cell head favorite'
+		<table className='table'>
+			<thead className='table-head'>
+				<tr className='table-row'>
+					<th
+						className='table-cell head'
 						onMouseEnter={() => setHoverColumn(0)}
 						onMouseLeave={() => setHoverColumn(null)}
 					>
 						<Star />
-					</div>
-					{columns.map((column, index) => (
-						<div
+					</th>
+					{tableColumns.map((column, index) => (
+						<th
 							className='table-cell head'
 							onMouseEnter={() => setHoverColumn(index + 1)}
 							onMouseLeave={() => setHoverColumn(null)}
@@ -84,37 +27,39 @@ export const TableEmploee = () => {
 						>
 							<p className='table-cell-text'>{column.label}</p>
 							<TableSort sort='asc' />
-						</div>
+						</th>
 					))}
-				</div>
-			</div>
-			<div className='table-body'>
-				<div className='table-row'>
-					<div
-						className='table-cell body favorite'
-						style={{
-							backgroundColor:
-								hoverColumn === 0 ? 'var(--gray-0)' : '',
-						}}
-					>
-						<Star />
-					</div>
-					{columns.map((asd, index) => (
-						<div
+				</tr>
+			</thead>
+			<tbody className='table-body'>
+				{tableRows.map(row => (
+					<tr className='table-row'>
+						<td
 							className='table-cell body'
 							style={{
 								backgroundColor:
-									hoverColumn === index + 1
-										? 'var(--gray-0)'
-										: '',
-								minWidth: asd.minWidth,
+									hoverColumn === 0 ? 'var(--gray-0)' : '',
 							}}
 						>
-							<p className='table-cell-text'>{asd.label}</p>
-						</div>
-					))}
-				</div>
-			</div>
-		</div>
+							<Star />
+						</td>
+						{Object.values(row).map((cell, index) => (
+							<td
+								className='table-cell body'
+								style={{
+									backgroundColor:
+										hoverColumn === index + 1
+											? 'var(--gray-0)'
+											: '',
+									minWidth: tableColumns[index].minWidth,
+								}}
+							>
+								<p className='table-cell-text'>{cell}</p>
+							</td>
+						))}
+					</tr>
+				))}
+			</tbody>
+		</table>
 	)
 }
